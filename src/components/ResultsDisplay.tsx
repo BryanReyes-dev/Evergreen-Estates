ac3f153beb761231b5819f5c0313eab2d82879e6
Bryan <bg478806@gmail.com>
2026-08-06 20:18:18 -0400

    style: update layout of ResultsDisplay component for improved responsiveness and spacing

--- src/components/ResultsDisplay.tsx
+++ src/components/ResultsDisplay.tsx
@@ -48,12 +48,13 @@ export const ResultsDisplay = async ({
  
 
     return (
-        <div className="grid grid-cols-3 gap-6">
+        <div className="  grid justify-center sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 p-4">
 
             {listings.map(listing => (
                 <Listing
                     key={listing.id}
                     listing={listing}
+                    
                 />
             ))}
 
