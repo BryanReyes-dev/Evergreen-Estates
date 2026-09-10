39735cfb890dc907bd40610e54023676a8a33427
Bryan Reyes <101713166+BryanReyes-dev@users.noreply.github.com>
2026-09-10 15:59:55 -0400

    fix: simplify listing media types

--- src/app/types.ts
+++ src/app/types.ts
@@ -34,28 +34,28 @@ export interface Review {
 }
 
 export interface FeaturedCommunitiesProps {
-    listings: Houselisting[]
+  listings: Houselisting[];
 }
 
 export type ListingMedia = {
-  type: "image" | "gif" | "video";
+  type: "image" | "video";
   src: string;
   alt?: string;
 };
-export  type ListingMediaCarouselProps = {
+
+export type ListingMediaCarouselProps = {
   media: ListingMedia[];
   options?: EmblaOptionsType;
 };
 
 export interface filters {
-    price: [number, number] | null
-    tags: string[] | null
-    search: string | null
+  price: [number, number] | null;
+  tags: string[] | null;
+  search: string | null;
 }
 
 export interface SearchParams {
-    query?: string;
-    price?: string;
-    tag?: string | string[];
+  query?: string;
+  price?: string;
+  tag?: string | string[];
 }
-
