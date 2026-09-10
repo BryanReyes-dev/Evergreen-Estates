cdb0eebed8feed409d1ffd7ddc3e93215186c9f0
Bryan Reyes <101713166+BryanReyes-dev@users.noreply.github.com>
2026-09-10 16:00:41 -0400

    fix: remove unused search page code

--- src/app/(site)/search/page.tsx
+++ src/app/(site)/search/page.tsx
@@ -1,34 +1,28 @@
-
 import { Filters } from "@/components/Filters";
-import Listing from "@/components/Listing";
 import { Suspense } from "react";
-import react from "react";
-import {ResultsDisplay} from "@/components/ResultsDisplay";
-
-const delay = (ms: number) => new Promise(resolve => setTimeout(resolve, ms));
+import { ResultsDisplay } from "@/components/ResultsDisplay";
 
 interface SearchParams {
-    query?: string;
-    price?: string;
-    tag?: string[];
+  query?: string;
+  price?: string;
+  tag?: string[];
 }
 
-const Search = async ({searchParams}: {searchParams: Promise<SearchParams>;}) => {
+const Search = async ({
+  searchParams,
+}: {
+  searchParams: Promise<SearchParams>;
+}) => {
+  const params = await searchParams;
 
-    const params = await searchParams;
-
-    console.log("PAGE PARAMS:", params);
-    return (
-        <div className=""> 
-
-            <Filters/>
-            
-        
-            <Suspense fallback={<div>Loading results...</div>}>
-                <ResultsDisplay searchParams={params} />
-            </Suspense>
-        </div>
-    );
+  return (
+    <div>
+      <Filters />
+      <Suspense fallback={<div>Loading results...</div>}>
+        <ResultsDisplay searchParams={params} />
+      </Suspense>
+    </div>
+  );
 };
 
 export default Search;
